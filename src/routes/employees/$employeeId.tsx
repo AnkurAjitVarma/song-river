d28e3a6879e993141305c16cf945ca33d5742c6e
Ankur Varma <ankur.a.varma@outlook.com>
2025-11-14 16:02:19 -0600

    setting up the loader

--- src/routes/employees/$employeeId.tsx
+++ src/routes/employees/$employeeId.tsx
@@ -1,6 +1,10 @@
-import { createFileRoute } from '@tanstack/react-router'
+import {createFileRoute} from '@tanstack/react-router'
 import Employee from "../../components/Employee.tsx";
 
 export const Route = createFileRoute('/employees/$employeeId')({
   component: Employee,
+  loader: async ({ params }) => {
+      const response = await fetch(`http://localhost:3030/employees/${params.employeeId}`)
+      return await response.json()
+  }
 })
