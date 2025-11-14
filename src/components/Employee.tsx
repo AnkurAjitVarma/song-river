c84418cd0c5b3a578695b87970544b8f12ac86fc
Ankur Varma <ankur.a.varma@outlook.com>
2025-11-14 16:26:12 -0600

    setting up the loader

--- src/components/Employee.tsx
+++ src/components/Employee.tsx
@@ -1,8 +1,9 @@
 import styles from "../styles/Employee.module.css"
+import {getRouteApi} from "@tanstack/react-router";
 
 const Employee = (): React.JSX.Element => {
-    return <div className={styles.Employee}>
-    </div>
+    const data = getRouteApi('/employees/$employeeId').useLoaderData()
+    return <div className={styles.Employee}>{JSON.stringify(data, null, 2)}</div>
 }
 
 export default Employee
