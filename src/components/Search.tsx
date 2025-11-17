ae61944ec88f616a04c3b4e555a1433965c1d5ca
Ankur Varma <ankur.a.varma@outlook.com>
2025-11-17 14:46:17 -0600

    making the search bar functional

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,15 +1,17 @@
-import type { JSX } from "react";
+import { type JSX } from "react";
 import styles from "../styles/Search.module.css"
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { MdClear } from "react-icons/md";
+import useSearchNavigation from "../hooks/useSearchNavigation.tsx";
 
 const Search = (): JSX.Element => {
+    const { inputRef, handleSearchChange, handleClearSearch } = useSearchNavigation();
     return <div className={styles.Search}>
         <span className={styles.Glass}>
             <FaMagnifyingGlass />
         </span>
-        <input className={styles.Input} type="search" />
-        <button className={styles.Clear} type="button">
+        <input ref={inputRef} className={styles.Input} type="search" onChange={handleSearchChange} />
+        <button className={styles.Clear} type="button" onClick={handleClearSearch}>
             <MdClear size={20} />
         </button>
     </div>
