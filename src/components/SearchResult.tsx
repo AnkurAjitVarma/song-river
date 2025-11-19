1366fa7ce7939906e3bacf285b3e4d4e80703fe7
Ankur Varma <ankur.a.varma@outlook.com>
2025-11-18 19:24:28 -0600

    displaying the search results

--- src/components/SearchResult.tsx
+++
@@ -1,9 +0,0 @@
-import type {JSX} from "react";
-import {getRouteApi} from "@tanstack/react-router";
-
-const SearchResult = (): JSX.Element => {
-    const results = getRouteApi('/').useLoaderData()
-    return <div>{JSON.stringify(results)}</div>
-}
-
-export default SearchResult;
