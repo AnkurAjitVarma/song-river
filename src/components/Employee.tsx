9f467021e761e52ca3c7ad89226c352c88649d03
Ankur Varma <ankur.a.varma@outlook.com>
2025-11-22 18:44:14 -0600

    using more semantically appropriate tags

--- src/components/Employee.tsx
+++ src/components/Employee.tsx
@@ -5,20 +5,20 @@ import type { Employee as EmployeeType } from "../schemas/employeeSchema.tsx";
 
 const Employee = (): JSX.Element => {
     const employee: EmployeeType = getRouteApi('/employees/$employeeId').useLoaderData()
-    return <div className={styles.Employee}>
+    return <article className={styles.Employee}>
             <div className={styles.Picture}>
                 <img src={employee.imageFilePath} alt="Profile Picture" />
             </div>
             <div className={styles.Info}>
-                <div className={styles.Name}>
+                <h1 className={styles.Name}>
                     <span className={styles.FirstName}>{employee.firstName}</span>
                     <span className={styles.LastName}>{employee.lastName}</span>
-                </div>
+                </h1>
                 <div className={styles.Designation}>
                     <span className={styles.Role}>{employee.jobTitle}</span>
                     <span className={styles.Team}>{employee.teamName}</span>
                 </div>
             </div>
-        </div>
+        </article>
 }
 export default Employee
